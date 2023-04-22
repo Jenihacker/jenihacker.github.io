7d0d21281104d2cc2d884b8bd3e49b21e96d4c9d
Jenihacker <20cs064.jenison@sjec.ac.in>
2023-04-22 22:03:02 +0530

    added google meta tag

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,7 +12,8 @@ function About() {
             <img src={ image } alt="image" width={300} height={300} draggable="false" />
         </div>
         <div className="abtsec">
-          <p>Hello, my name is Jenison Monteiro and I'm a Web Developer from Mangalore.I've been fascinated with computers and the internet since I was a kid, and I've always loved building things with code. I love to work with computers and also explore new things.When I'm not coding, I love to indulge in my hobby of photo and video editing.</p>
+          <p>Hello, my name is Jenison Monteiro and I'm a Web Developer from Mangalore. I've been fascinated with computers and the internet since I was a kid, and I've always loved building things with code. I love to work with computers and also explore new things.When I'm not coding, I love to indulge in my hobby of photo and video editing.</p>
+          <br />
           <p>I believe that being a web developer is more than just a job – it's a lifestyle. I'm constantly seeking out new challenges and opportunities to improve my skills.</p>
         </div>
       </div>
